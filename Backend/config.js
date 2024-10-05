11e728532ceefd2252fe8f37f4e208de299390a2
Aaditya Anil K <60652183+aaditya-anil@users.noreply.github.com>
2024-10-05 15:54:11 +0530

    Update config.js

--- Backend/config.js
+++ Backend/config.js
@@ -2,4 +2,4 @@ export const PORT=5555;
 export const MongoUserName = "";
 export const MongoPassword = "";
 
-export const mongoDBUrl = `mongodb+srv://${MongoUserName}:${MongoPassword}@todo-app-mern.kjvbt.mongodb.net/Todo-list-collection?retryWrites=true&w=majority&appName=Todo-app-MERN`
+export const mongoDBUrl = ``
